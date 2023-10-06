83c77ec6969da5b651942d2dff2fabbbcd466ca6
cobra1294 <139133937+cobra1294@users.noreply.github.com>
2023-10-06 18:50:26 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -19,6 +19,7 @@ class Config(object):
 	BROADCAST_AS_COPY = bool(os.environ.get("BROADCAST_AS_COPY", False))
 	BANNED_CHAT_IDS = list(set(int(x) for x in os.environ.get("BANNED_CHAT_IDS", "-1001362659779 -1001255795497").split()))
 	OTHER_USERS_CAN_SAVE_FILE = bool(os.environ.get("OTHER_USERS_CAN_SAVE_FILE", True))
+	PORT = environ.get("PORT", "8080")
 	ABOUT_BOT_TEXT = f"""
 This is a Permanent FileStore Bot. 
 Send Me any Media or File. I can Work In Channel too. Add Me to Channel with Edit Permission, I will add save Uploaded File in Channel and Share a Shareable Link. 
